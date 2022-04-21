aa37bb7797cd97a717edef5fecb0a1dd4f4223e7
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-04-20 19:46:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Alejandro is the champion";
+document.body.append(newHeader);
